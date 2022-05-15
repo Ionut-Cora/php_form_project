e3b6f3a2f88c0ca68d0077d53573a49c6ef00a86
Ionut Cora <cora.ionut92@yahoo.com>
2022-05-15 14:53:12 +0100

    menu implemetation

--- index.php
+++ index.php
@@ -12,15 +12,33 @@
     <header id="banner"></header>
     <nav id="menu">
         <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Login</a></li>
-            <li><a href="#">Register</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="index.php?page=1">Login</a></li>
+            <li><a href="index.php?page=2">Register</a></li>
         </ul>
     </nav>
     <section id="content">
+
         <?php
-            
+            if (isset($_GET['page'])) {
+                $page = $_GET['page'];
+
+                switch ($page) {
+                    case 1:
+                        require_once 'pages/login.php';
+                        break;
+                    case 2:
+                        require_once 'pages/register.php';
+                        break;
+                    default:
+                        require_once 'pages/error.php';
+                        break;
+                }
+            } else {
+                require_once 'pages/home.php';
+            }
         ?>
+
     </section>
     
 </body>
