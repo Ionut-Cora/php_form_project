f598a7e6c0206288de2e8bcc5237f88ae6e46439
Ionut Cora <cora.ionut92@yahoo.com>
2022-05-15 15:08:57 +0100

    login page form made

--- pages/login.php
+++ pages/login.php
@@ -1,2 +1,24 @@
 
 <h1>Login</h1>
+
+<form methode="post">
+    <table>
+        <tr>
+            <td><label for="email">Email</label></td>
+            <td>
+                <input type="email" id="email" name="email"/>
+            </td>
+        </tr>
+        <tr>
+            <td><label for="pass">Password</label></td>
+            <td>
+                <input type="password" id="pass" name="pass"/>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="submit" name="login" value="Login">
+            </td>
+        </tr>
+    </table>
+</form>
