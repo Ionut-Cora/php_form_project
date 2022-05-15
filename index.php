8edc07e6bce2da2dbc940665c48fbfca12cf2005
Ionut Cora <cora.ionut92@yahoo.com>
2022-05-15 14:31:22 +0100

    pages create

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Project</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+    <header id="banner"></header>
+    <nav id="menu">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Register</a></li>
+        </ul>
+    </nav>
+    <section id="content">
+        <?php
+            
+        ?>
+    </section>
+    
+</body>
+</html>
